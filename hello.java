7023729fa55c3b200bce31aa633d41cc5c42a51c
surya <136917348+suryapalanisamy40@users.noreply.github.com>
2023-07-07 17:12:23 +0530

    im overrideing irrelevent code

--- hello.java
+++ hello.java
@@ -1,9 +1,7 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
-	  System.out.println("hjghjghjkjhjkhkhkhkjhhhhiuyh");
 	  System.out.println("new branch change commit 2");
 	  
-	  "sdiugfuks"
    }
 }
